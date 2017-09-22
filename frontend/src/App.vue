c9708a09efddfc17d5757a5fae92bfca51502a26
Quy Le <lnquy.it@gmail.com>
2017-09-23 00:28:28 +0700

    Styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -41,18 +41,17 @@
                     <h3>{{ tbl.name }}</h3>
                     <table class="fg-res-table">
                         <thead>
-                            <tr>
-                                <th>Field</th>
-                                <th>Size</th>
-                                <th>Memory alignment</th>
-                            </tr>
+                        <tr>
+                            <th class="text-center">Field</th>
+                            <th class="text-center">Size</th>
+                            <th>Memory alignment</th>
+                        </tr>
                         </thead>
                         <tr v-for="f in tbl.fields">
-                            <td>{{ f.name }}</td>
-                            <td>{{ f.size }}</td>
+                            <td class="text-center">{{ f.name }}</td>
+                            <td class="text-center">{{ f.size }}</td>
                             <td style="display: flex; flex-wrap:wrap;">
                                 <div v-for="i in f.index" class="fg-box fg-index-box"></div>
-
                                 <span v-if="f.size <= getChunkByte()">
                                     <div v-for="i in f.size" class="fg-box fg-size-box"></div>
                                 </span>
@@ -95,7 +94,7 @@
                     foldGutter: true,
                     gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
                     styleSelectedText: true,
-                    highlightSelectionMatches: { showToken: /\w/, annotateScrollbar: true },
+                    highlightSelectionMatches: {showToken: /\w/, annotateScrollbar: true},
                 }
             }
         },
@@ -124,10 +123,10 @@
             }
         },
         watch: {
-            'fuguForm.language': function() {
+            'fuguForm.language': function () {
                 this.res_table = []
             },
-            'fuguForm.arch': function() {
+            'fuguForm.arch': function () {
                 this.res_table = []
             },
         }
@@ -164,11 +163,6 @@
     .fg-form {
     }
 
-    .fg-res-table {
-        width: 100%;
-        height: 100%;
-    }
-
     .fg-box {
         width: 15px;
         height: 15px;
@@ -176,53 +170,135 @@
     }
 
     .fg-index-box {
-        background-color: gray;
+        background-color: #BDBDBD;
     }
 
     .fg-size-box {
-        background-color: green;
+        background-color: #4CAF50;
         display: inline-flex;
         flex-wrap: wrap;
     }
 
     .fg-padding-box {
-        background-color: red;
+        background-color: #F44336;
     }
 
-    .codemirror{
+    .codemirror {
         font-size: 14px;
         line-height: 1.5em;
     }
 
-    table {
-        max-width: 960px;
-        margin: 10px auto;
+    /*** Table Styles **/
+
+    .fg-res-table {
+        background: white;
         border-radius: 4px;
+        border-collapse: collapse;
+        width: 100%;
+        height: 100%;
+        box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
+        animation: float 5s infinite;
+        text-align: left;
     }
 
-    thead th {
-        font-weight: 400;
-        background: #8a97a0;
-        color: #FFF;
+    th {
+        color: #D5DDE5;;
+        background: #1b1e24;
+        /*border-bottom: 4px solid #9ea7af;*/
+        border-right: 1px solid #343a45;
+        font-size: 16px;
+        font-weight: 100;
+        padding: 20px;
+        text-align: left;
+        text-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+        vertical-align: middle;
+    }
+
+    th:first-child {
+        border-top-left-radius: 3px;
+    }
+
+    th:last-child {
+        border-top-right-radius: 3px;
+        border-right: none;
     }
 
     tr {
-        background: #f4f7f8;
-        border-bottom: 1px solid #FFF;
-        margin-bottom: 5px;
+        border-top: 1px solid #C1C3D1;
+        border-bottom-: 1px solid #C1C3D1;
+        color: #666B85;
+        font-size: 14px;
+        font-weight: normal;
+        text-shadow: 0 1px 1px rgba(256, 256, 256, 0.1);
     }
 
-    tr:nth-child(even) {
-        background: #e8eeef;
+    /*tr:hover td {*/
+        /*background: #4E5066;*/
+        /*color: #FFFFFF;*/
+        /*border-top: 1px solid #22262e;*/
+    /*}*/
+
+    tr:first-child {
+        border-top: none;
     }
 
-    th, td {
-        text-align: left;
-        padding: 20px;
-        font-weight: 300;
+    tr:last-child {
+        border-bottom: none;
+    }
+
+    tr:nth-child(odd) td {
+        background: #EBEBEB;
+    }
+
+    /*tr:nth-child(odd):hover td {*/
+        /*background: #4E5066;*/
+    /*}*/
+
+    tr:last-child td:first-child {
+        border-bottom-left-radius: 3px;
+    }
+
+    tr:last-child td:last-child {
+        border-bottom-right-radius: 3px;
     }
 
     td {
+        background: #FFFFFF;
         padding: 10px 20px;
+        text-align: left;
+        vertical-align: middle;
+        font-weight: 300;
+        font-size: 16px;
+        text-shadow: -1px -1px 1px rgba(0, 0, 0, 0.1);
+        border-right: 1px solid #C1C3D1;
     }
+
+    td:last-child {
+        border-right: 0;
+    }
+
+    th.text-left {
+        text-align: left;
+    }
+
+    th.text-center {
+        text-align: center;
+    }
+
+    th.text-right {
+        text-align: right;
+    }
+
+    td.text-left {
+        text-align: left;
+    }
+
+    td.text-center {
+        text-align: center;
+    }
+
+    td.text-right {
+        text-align: right;
+    }
+
 </style>
