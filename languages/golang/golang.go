ec293c3cc210706b2393c31a0d31fb729a52a54e
Quy Le Ngoc <lnquy.it@gmail.com>
2017-09-22 17:14:34 +0700

    Done calculate Go struct

--- languages/golang/golang.go
+++ languages/golang/golang.go
@@ -1,45 +1,161 @@
 package golang
 
 import (
+	"fmt"
 	"github.com/lnquy/fugu/modules/global"
-	"strings"
 	"github.com/lnquy/fugu/modules/util"
-	"github.com/rs/xid"
-	log "github.com/sirupsen/logrus"
+	"regexp"
+	"strings"
+	"time"
 )
 
-type Golang struct {}
+type Abc_Def struct {
+}
+
+type (
+	Golang struct{}
+
+	Field struct {
+		Name    string
+		Type    string
+		Size    uint8
+		Index   uint8
+		Padding uint8
+	}
+)
 
 func (g *Golang) CalculateSizeof(data string, arch global.Architecture) (string, error) {
-	parseData(data)
-	log.Infof("Map: %v", s)
+	s := parseData(data)
+	for _, v := range s {
+		getFieldSizes(v, arch)
+		calcPadding(v, arch)
+	}
+
+	for k, v := range s {
+		fmt.Printf("%v\n", k)
+		for _, f := range v {
+			fmt.Printf("    %v\n", f)
+		}
+	}
 	return "Go", nil
 }
 
 var (
-	s = make(map[string][]string)
+	sNameRegex  = regexp.MustCompile(`\A\s*type\s+([a-zA-Z0-9_]*\s+)?struct\s*{\s*\z`)
+	sFieldRegex = regexp.MustCompile(`\A\s*([a-zA-Z_][a-zA-Z0-9_]*)\s+([a-zA-Z0-9_\[\]\{\}\<\-\>*]+(\s+[a-zA-Z0-9_\[\]\{\}\<\-\>*]+)?)\s*\z`)
+	sEndRegex   = regexp.MustCompile(`\A\s*}\s*\z`)
 )
 
-func parseData(data string) {
+func parseData(data string) map[string][]*Field {
+	s := make(map[string][]*Field)
+	curStruct := ""
+
 	lines := strings.Split(data, util.LineBreak())
-	var curStruct string
 	for _, line := range lines {
-		line = strings.TrimSpace(line)
-		line = strings.Trim(line, "\t")
-		if strings.HasPrefix(line, "type ") { // struct type
-			name := line[5:strings.Index(line, "struct")]
-			if name == "" {
-				name = xid.New().String() // TODO
+		if sNameRegex.MatchString(line) {
+			curStruct = sNameRegex.FindStringSubmatch(line)[1]
+			if curStruct == "" {
+				curStruct = fmt.Sprintf("%v", time.Now().Unix())
 			}
-			curStruct = name
-			s[curStruct] = make([]string, 0)
+			s[curStruct] = make([]*Field, 0)
 			continue
 		}
-		if line == "}" {
+		if sEndRegex.MatchString(line) {
+			curStruct = ""
 			continue
 		}
-		if curStruct != "" {
-			s[curStruct] = append(s[curStruct], line)
+		if sFieldRegex.MatchString(line) {
+			m := sFieldRegex.FindStringSubmatch(line)
+			s[curStruct] = append(s[curStruct], &Field{
+				Name: m[1],
+				Type: m[2],
+			})
+		}
+	}
+	return s
+}
+
+func getFieldSizes(fields []*Field, arch global.Architecture) {
+	for _, f := range fields {
+		if f.Type == "struct{}" || strings.HasPrefix(f.Type, "[0]") {
+			f.Size = 0
+			continue
+		}
+
+		if strings.Contains(f.Type, "8") || f.Type == "bool" || f.Type == "byte" {
+			f.Size = uint8(1)
+			continue
+		}
+
+		if strings.Contains(f.Type, "16") {
+			f.Size = uint8(2)
+			continue
+		}
+
+		if strings.Contains(f.Type, "32") || f.Type == "rune" {
+			f.Size = uint8(4)
+			continue
+		}
+
+		if strings.Contains(f.Type, "64") ||
+			strings.HasPrefix(f.Type, "*") ||
+			f.Type == "uintptr" ||
+			strings.Contains(f.Type, "chan") ||
+			strings.HasPrefix(f.Type, "map") ||
+			strings.HasPrefix(f.Type, "func") ||
+			strings.HasPrefix(f.Type, "func") {
+			f.Size = uint8(8)
+			continue
+		}
+
+		if strings.Contains(f.Type, "128") || f.Type == "string" {
+			f.Size = uint8(16)
+			continue
+		}
+
+		if strings.HasPrefix(f.Type, "[]") {
+			f.Size = uint8(24)
+			continue
+		}
+
+		if f.Type == "int" || f.Type == "uint" {
+			if arch == global.I386 {
+				f.Size = uint8(4)
+				continue
+			}
+			if arch == global.Amd64 {
+				f.Size = uint8(8)
+				continue
+			}
+		}
+	}
+}
+
+func calcPadding(fields []*Field, arch global.Architecture) {
+	chunk := uint8(4)
+	if arch == global.Amd64 {
+		chunk = uint8(8)
+	}
+
+	for i, f := range fields {
+		lastBits := f.Size % chunk
+		if lastBits == 0 {
+			lastBits = chunk
+		}
+		if i == 0 {
+			f.Index = 0
+		}
+		if i == len(fields)-1 {
+			f.Padding = chunk - lastBits
+			continue
+		}
+		next := fields[i+1]
+		if f.Index+f.Size+next.Size > chunk {
+			f.Padding = chunk - lastBits - f.Index
+			next.Index = 0
+		} else {
+			f.Padding = 0
+			next.Index = f.Index + f.Size
 		}
 	}
 }
--- languages/languages.go
+++ languages/languages.go
@@ -3,7 +3,6 @@ package languages
 import (
 	"fmt"
 	"github.com/lnquy/fugu/modules/global"
-	log "github.com/sirupsen/logrus"
 
 	cpl "github.com/lnquy/fugu/languages/c_cpp"
 	gopl "github.com/lnquy/fugu/languages/golang"
@@ -26,7 +25,6 @@ func init() {
 }
 
 func CalcSizeOf(data string, lang global.Language, arch global.Architecture) (string, error) {
-	log.Infof("plangs: %v - Lang: %s", pLangs, lang.String())
 	pl, ok := pLangs[lang.String()]
 	if !ok {
 		return "", fmt.Errorf("fugu: %s language is not supported yet", lang.String())
